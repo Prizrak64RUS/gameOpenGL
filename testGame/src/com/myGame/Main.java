5c4017ced39caaa0a38032b5e3ad888aa59ce97c
Prizrak64RUS <Prizrak64RUS@gmail.com>
2015-09-16 14:58:13 +0300

    2

--- testGame/src/com/myGame/Main.java
+++ testGame/src/com/myGame/Main.java
@@ -3,10 +3,13 @@ package com.myGame;
 /**
  * Created by user on 16.09.15.
  */
+import com.myGame.renderEngine.DisplayMenager;
+
 import static org.lwjgl.opengl.GL45.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        SharedLibraryLoader.load();
+        new   DisplayMenager().createDisplay();
     }
 }
