d5f3c5059837e01c7d53e8d22233e103c7dc5937
Prizrak64RUS <Prizrak64RUS@gmail.com>
2015-09-16 11:33:49 +0300

    create

---
+++ testGame/src/com/myGame/Main.java
@@ -0,0 +1,12 @@
+package com.myGame;
+
+/**
+ * Created by user on 16.09.15.
+ */
+import static org.lwjgl.opengl.GL45.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
